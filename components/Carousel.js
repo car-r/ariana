3a2e5e3833251a3371ce6cee7f869e2f6395dadd
Carson Richter <carson.ri@protonmail.com>
2022-08-18 15:48:32 -0600

    custom xs media query + all other mq to assist responsive carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -9,11 +9,11 @@ import Link from "next/link"
 export default function ContentCarousel({posts}) {
     console.log(posts)
     return (
-        <div className="w-full flex flex-col dark:bg-black py-32">
+        <div className="w-full flex flex-col bg-gray-100 dark:bg-stone-900 py-40">
             <h2 className="text-3xl md:text-3xl text-center w-11/12 max-w-6xl mx-auto mb-6 font-semibold">
                 Recent Posts
             </h2>
-            <div className="mx-auto relative w-11/12 md:w-3/4 lg:w-3/5 h-44 sm:h-80 md:h-96">
+            <div className="mx-auto relative w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 h-52 xs:h-72 sm:h-80 md:h-96">
                 <Carousel>
                     {posts.map((post) => (
                         <Link href={`/posts/${post.slug}`} key={post.slug}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {},
+    screens: {
+      'xs': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
